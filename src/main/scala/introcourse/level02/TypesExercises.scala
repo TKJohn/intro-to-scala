62f6b7f6f581cbb5b21b0540699228b60bc3c2bb
TKJohn <fzwjohnfan@gmail.com>
2019-08-16 01:17:38 +0800

    2. Type

--- src/main/scala/introcourse/level02/TypesExercises.scala
+++ src/main/scala/introcourse/level02/TypesExercises.scala
@@ -49,7 +49,7 @@ object TypesExercises {
     **/
   def showPerson1(person: Person): String =
     person match {
-      case Person(name, age) => s"${???} is ${???} years old"
+      case Person(name, age) => s"$name is $age years old"
     }
 
   /**
@@ -58,7 +58,7 @@ object TypesExercises {
     * Hint: Navigate the Person class' fields using the "." operator
     */
   def showPerson2(person: Person): String =
-    s"${???} is ${???} years old"
+    s"${person.name} is ${person.age} years old"
 
   /**
     * scala> val person = Person("Bob", 50)
@@ -70,7 +70,7 @@ object TypesExercises {
     *
     * Hint: Use the .copy method
     */
-  def changeName(newName: String, person: Person): Person = ???
+  def changeName(newName: String, person: Person): Person = person.copy(name = newName)
 
   /**
     * Let's look at another data type.
@@ -93,7 +93,7 @@ object TypesExercises {
     *
     * You can solve this like how you solved `showPerson1` or `showPerson2`.
     */
-  def showWallet(wallet: Wallet): String = ???
+  def showWallet(wallet: Wallet): String = s"The wallet amount is ${wallet.amount}"
 
   /**
     * Here is another example of working with immutable values.
@@ -104,7 +104,7 @@ object TypesExercises {
     *
     * Hint: You need to calculate the new amount first.
     **/
-  def purchase(cost: Double, wallet: Wallet): Wallet = ???
+  def purchase(cost: Double, wallet: Wallet): Wallet = wallet.copy(wallet.amount - cost)
 
   /**
     * *********************************************
@@ -138,7 +138,12 @@ object TypesExercises {
   /**
     * Implement the following showTrafficLightStr function to pass all your tests!
     */
-  def showTrafficLightStr(trafficLight: String): String = ???
+  def showTrafficLightStr(trafficLight: String): String = {
+    if (trafficLight == "red" || trafficLight == "yellow" || trafficLight == "green" || trafficLight == "flashing")
+      s"The traffic light is $trafficLight"
+    else
+      s"The traffic light is invalid"
+  }
 
 
   /**
@@ -151,7 +156,7 @@ object TypesExercises {
     * We need to have a new traffic light called Flashing:
     *
     * 1. Implement the test for this scenario: "should show flashing"
-    *    it should return "the traffic light is flashing"
+    * it should return "the traffic light is flashing"
     *
     * 2. Extend `showTrafficLightStr` that you have just implemented above to support this new flashing functionality.
     *
@@ -185,6 +190,8 @@ object TypesExercises {
 
   case object Green extends TrafficLight
 
+  case object Flashing extends TrafficLight
+
   /**
     * scala> showTrafficLight(Red)
     * = "The traffic light is red"
@@ -200,7 +207,16 @@ object TypesExercises {
     * Hint: Use pattern matching
     **/
 
-  def showTrafficLight(trafficLight: TrafficLight): String = ???
+  def showTrafficLight(trafficLight: TrafficLight): String = {
+    val lightStr = trafficLight match {
+      case Red => "red"
+      case Yellow => "yellow"
+      case Green => "green"
+      case Flashing => "flashing"
+      case _ => "invalid"
+    }
+    s"The traffic light is $lightStr"
+  }
 
   /**
     * *********************************************************
